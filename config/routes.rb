ba284bbf2e4fbba2f0b2f15b7cf9eca89d35dde6
Wellington Oliveira <wellington.calor@gmail.com>
2021-11-15 14:45:30 +0000

    Coach working
    Signed-off-by: Wellington Oliveira <wellington.calor@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.htm
+
+  get 'ask', to: 'questions#ask'
+  get 'answer', to: 'questions#answer'
+  # get 'home', to: 'questions#home'
 end
